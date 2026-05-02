9e634f8f2db5784a15c23bc5f45be42df185137f
AdlePy <askaradilet41@gmail.com>
2026-05-02 13:16:51 +0500

    added functions section

--- functions.js
+++ functions.js
@@ -20,3 +20,42 @@ function addition(firstNum, secondNum) {
 
 let result = addition(1, 2);
 console.log("result:", result);
+console.log(addition);
+
+function callFunction(func) {
+  console.log("before");
+  func("Kyle");
+  console.log("after");
+}
+
+callFunction(printName);
+
+function sumCallback(a, b, callback) {
+  let sum = a + b;
+  callback(sum);
+}
+
+function handleAddition(sum) {
+  console.log(sum);
+}
+
+sumCallback(1, 2, handleAddition);
+
+function greet(name) {
+  return "Hello, " + name;
+}
+
+function greetPerson(firstName, lastName, callback) {
+  let fullName = firstName + " " + lastName;
+  return greet(fullName);
+}
+
+const greeting = greetPerson("Adilet", "Askar", greet);
+console.log(greeting);
+
+// arrow functions
+const sumArrow = (a, b) => a + b;
+console.log(sumArrow(3, 6));
+
+const printNameShort = (name) => console.log(name);
+printNameShort("Adle");
