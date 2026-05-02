fb456cebf094b3807dcb32447a3a36e88f3eea4d
AdlePy <askaradilet41@gmail.com>
2026-05-02 13:54:41 +0500

    upadated variables section

--- variables.js
+++ variables.js
@@ -1,12 +1,31 @@
-let myName = "Adilet";
-myName = "Askar Adilet";
-console.log(myName);
+// let myName = "Adilet";
+// myName = "Askar Adilet";
+// console.log(myName);
 
-const count = 1;
-console.log(count);
+// const count = 1;
+// console.log(count);
 
-let undedinedVar = undefined;
-console.log(undedinedVar);
+// let undedinedVar = undefined;
+// console.log(undedinedVar);
 
-let nullVar = null;
-console.log(nullVar);
+// let nullVar = null;
+// console.log(nullVar);
+
+// var myVar = 1;
+// console.log(myVar);
+
+// var uses function scope
+function func() {
+  {
+    var a = 1;
+  }
+
+  console.log(a);
+}
+
+func();
+
+// var variables are hoisted
+console.log(dog);
+var dog = "Black";
+console.log(dog);
